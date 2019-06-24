5ec8c87425e0abb00c58f212778e4f764fb5e165
Daniel Abbatt <daniel.abbatt@panoramic.systems>
2019-06-24 18:37:10 +0100

    Added debug for viewing actual responses in debug window only

--- Certify.Api/Http/AuthenticatedHttpClientHandler.cs
+++ Certify.Api/Http/AuthenticatedHttpClientHandler.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,7 +23,45 @@ namespace Certify.Api.Http
 		{
 			request.Headers.Add("x-api-key", apiKey);
 			request.Headers.Add("x-api-secret", apiSecret);
-			return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+			var response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+#if DEBUG
+			var requestId = Guid.NewGuid();
+			Debug.WriteLine($"{requestId} - {response.StatusCode} ({(int)response.StatusCode}) '{response.ReasonPhrase}'");
+			if (response.Content != null)
+			{
+				foreach (var header in response.Content.Headers)
+				{
+					Debug.WriteLine($"{requestId} {header.Key}: {string.Join(", ", header.Value)}");
+				}
+
+				if (response.Content is StringContent || IsTextBasedContentType(response.Headers) || IsTextBasedContentType(response.Content.Headers))
+				{
+					var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+					Debug.WriteLine($"{requestId} Content:");
+					Debug.WriteLine($"{requestId} {result}");
+					// Use below for more truncated version
+					//Debug.WriteLine($"{requestId} {string.Join("", result.Cast<char>().Take(255))}...");
+				}
+			}
+#endif
+			return response;
 		}
+
+#if DEBUG
+		private readonly string[] types = new[] { "html", "text", "xml", "json", "txt", "x-www-form-urlencoded" };
+
+		private bool IsTextBasedContentType(HttpHeaders headers)
+		{
+			if (!headers.TryGetValues("Content-Type", out var values))
+			{
+				return false;
+			}
+
+			var header = string.Join(" ", values).ToLowerInvariant();
+
+			return types.Any(t => header.Contains(t));
+		}
+#endif
 	}
 }
