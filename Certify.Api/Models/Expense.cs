bc892985cc77a05ea95e5ac30b889562571b92f7
David Bond <david.bond@panoramicdata.com>
2019-08-13 20:34:13 +0100

    Added missing properties to Expense

--- Certify.Api/Models/Expense.cs
+++ Certify.Api/Models/Expense.cs
@@ -182,133 +182,133 @@ namespace Certify.Api.Models
 		/// </summary>
 		[DataMember(Name = "ExpenseTextGLD2Label")]
 		public string ExpenseTextGld2Label { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseTextGLD2
 		/// </summary>
 		[DataMember(Name = "ExpenseTextGLD2")]
 		public string ExpenseTextGld2 { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseTextGLD3Label
 		/// </summary>
 		[DataMember(Name = "ExpenseTextGLD3Label")]
 		public string ExpenseTextGld3Label { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseTextGLD3
 		/// </summary>
 		[DataMember(Name = "ExpenseTextGLD3")]
 		public string ExpenseTextGld3 { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseTextGLD4Label
 		/// </summary>
 		[DataMember(Name = "ExpenseTextGLD4Label")]
 		public string ExpenseTextGld4Label { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseTextGLD4
 		/// </summary>
 		[DataMember(Name = "ExpenseTextGLD4")]
 		public string ExpenseTextGld4 { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseTextGLD5Label
 		/// </summary>
 		[DataMember(Name = "ExpenseTextGLD5Label")]
 		public string ExpenseTextGld5Label { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseTextGLD5
 		/// </summary>
 		[DataMember(Name = "ExpenseTextGLD5")]
 		public string ExpenseTextGld5 { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD1Label
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD1Label")]
 		public string ExpenseReportGld1Label { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD1Name
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD1Name")]
 		public string ExpenseReportGld1Name { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD1Code
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD1Code")]
 		public string ExpenseReportGld1Code { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD2Label
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD2Label")]
 		public string ExpenseReportGld2Label { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD2Name
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD2Name")]
 		public string ExpenseReportGld2Name { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD2Code
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD2Code")]
 		public string ExpenseReportGld2Code { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD3Label
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD3Label")]
 		public string ExpenseReportGld3Label { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD3Name
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD3Name")]
 		public string ExpenseReportGld3Name { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD3Code
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD3Code")]
 		public string ExpenseReportGld3Code { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD4Label
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD4Label")]
 		public string ExpenseReportGld4Label { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD4Name
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD4Name")]
 		public string ExpenseReportGld4Name { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD4Code
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD4Code")]
 		public string ExpenseReportGld4Code { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD5Label
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD5Label")]
 		public string ExpenseReportGld5Label { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD5Name
 		/// </summary>
 		[DataMember(Name = "ExpenseReportGLD5Name")]
 		public string ExpenseReportGld5Name { get; set; }
-		
+
 		/// <summary>
 		///  ExpenseReportGLD5Code
 		/// </summary>
@@ -350,5 +350,29 @@ namespace Certify.Api.Models
 		/// </summary>
 		[DataMember(Name = "CCLast")]
 		public string CcLast { get; set; }
+
+		/// <summary>
+		///  Location
+		/// </summary>
+		[DataMember(Name = "Location")]
+		public string Location { get; set; }
+
+		/// <summary>
+		///  ExpenseReportName
+		/// </summary>
+		[DataMember(Name = "ExpenseReportName")]
+		public string ExpenseReportName { get; set; }
+
+		/// <summary>
+		///  Status
+		/// </summary>
+		[DataMember(Name = "Status")]
+		public string Status { get; set; }
+
+		/// <summary>
+		///  LastAction
+		/// </summary>
+		[DataMember(Name = "LastAction")]
+		public string LastAction { get; set; }
 	}
 }
